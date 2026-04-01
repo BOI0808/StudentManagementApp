cbfcc8a9431bb157a2cb5acf1ad10f05c5e1f8e2
VietVinh <nguyenvietvinh22062006@gmail.com>
2026-04-01 23:28:41 +0700

    final test

--- android/backend/routes/gradeRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const gradeController = require("../controllers/gradeController");
-
-// Nhập điểm
-router.post("/nhap-diem", gradeController.nhapDiem);
-
-module.exports = router;
