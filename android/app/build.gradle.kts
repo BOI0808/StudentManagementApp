0a847f7c57cb87e881ca762e0193cf1eabd1802d
BOI0808 <nhatkhoi30@gmail.com>
2026-04-02 10:56:34 +0700

    Cap nhat API tiep nhan hoc sinh

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.studentmanagementapp"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.studentmanagementapp"
