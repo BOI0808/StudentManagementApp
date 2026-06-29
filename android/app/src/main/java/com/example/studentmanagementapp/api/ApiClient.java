28d25fe0c573d980464d38bd178a795a03e24583
BOI0808 <nhatkhoi30@gmail.com>
2026-06-29 18:38:29 +0700

    Fix lỗi sĩ số tối đa

--- android/app/src/main/java/com/example/studentmanagementapp/api/ApiClient.java
+++ android/app/src/main/java/com/example/studentmanagementapp/api/ApiClient.java
@@ -22,7 +22,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String BASE_URL = "http://10.0.2.2:3000/";
+    private static final String BASE_URL = "http://172.20.10.2:3000/";
     private static Retrofit retrofit = null;
     private static final Object lock = new Object();
 
